1eca4d84e4bc4fbc4de3a00370ce522decd33dee
Alex Buchanan <buchanae@gmail.com>
2013-06-27 13:17:12 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mesita',
+    description='TODO',
+    long_description=open('README.md').read(),
+    version='0.1.0',
+    author='Alex Buchanan',
+    author_email='buchanae@gmail.com',
+    license='BSD',
+    py_modules=['mesita'],
+)
