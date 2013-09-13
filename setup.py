69bac37ecb187559b385be3f1df38631bc390158
Alex Buchanan <buchanae@gmail.com>
2013-09-13 13:46:53 -0700

    version 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='mesita',
     description='TODO',
     long_description=open('README.md').read(),
-    version='0.1.0',
+    version='1.0.0',
     author='Alex Buchanan',
     author_email='buchanae@gmail.com',
     license='BSD',
