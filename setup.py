19a8a2d2133b53c8ea2cf230fe7194a095f79527
Alex Buchanan <buchanae@gmail.com>
2013-12-17 15:15:18 -0800

    license and bump version

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from distutils.core import setup
+import os
+
+_this_dir = os.path.dirname(__file__)
+readme_path = os.path.join(_this_dir, 'README.md')
+readme = open(readme_path).read()
 
 setup(
     name='mesita',
-    description='TODO',
-    long_description=open('README.md').read(),
-    version='1.0.0',
+    description='Simple, stupid utilitiy for working with tabular data',
+    long_description=readme,
+    version='1.0.1',
     author='Alex Buchanan',
     author_email='buchanae@gmail.com',
-    license='BSD',
+    license='MIT',
     py_modules=['mesita'],
 )
